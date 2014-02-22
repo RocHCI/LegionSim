05936647246f3ddde8bf9f42701f70747574b0bd
Walter <walterlasecki@wsl-mbp2013.rochester.rr.com>
2014-02-21 21:27:21 -0500

    cleaned game code

--- consistency/game.rb
+++ consistency/game.rb
@@ -14,94 +14,86 @@ actionSet = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
 puts("Tenure_Length,Random-Agent,Fixed-Agent,Crowd-Agent")
 while( @Tenure <= 1000 )
 
-##  MAIN  ##
-randAgent = RandomAgent.new(actionSet)
-fixedAgent = FixedAgent.new(actionSet)
-crowdAgent = CrowdAgent.new(actionSet)
+	##  MAIN  ##
+	randAgent = RandomAgent.new(actionSet)
+	fixedAgent = FixedAgent.new(actionSet)
+	crowdAgent = CrowdAgent.new(actionSet)
 
 
-# Logging arrays
-rResultSet = Array.new
-fResultSet = Array.new
-cResultSet = Array.new
+	# Logging arrays
+	rResultSet = Array.new
+	fResultSet = Array.new
+	cResultSet = Array.new
 
-# Test loop
-idx = 0
-while( idx < @NumRounds )
+	# Test loop
+	idx = 0
+	while( idx < @NumRounds )
 
-#puts("=======================")
-#puts("ROUND NUMBER ##{idx}")
-#puts("=======================")
 
-	# NOTE: Turnover position OPTION #2
+# NOTE: Turnover position OPTION #2
 =begin
-	if( idx % @Tenure == 0 )
-		randAgent.rotateWorker()
-		crowdAgent.rotateWorker()
-	end
-=end
-
-	stepIdx = 0
-	rActions = Array.new
-	fActions = Array.new
-	cActions = Array.new
-	while( stepIdx < @NumSteps )
-
-#puts("=======================")
-#puts("STEP NUMBER ##{stepIdx}")
-#puts("=======================")
-
-# NOTE: Turnover position OPTION #1
-#=begin
-		if( stepIdx % @Tenure == 0 )
+		if( idx % @Tenure == 0 )
 			randAgent.rotateWorker()
-			fixedAgent.rotateWorker()
 			crowdAgent.rotateWorker()
 		end
-#=end
-
-		rAct = randAgent.getMove()
-		fAct = fixedAgent.getMove()
-		cAct = crowdAgent.getMove()
-		rActions << rAct
-		fActions << fAct
-		cActions << cAct
-
-=begin
-		puts("\nAgent R: #{randAgent.getMove()}")
-		puts("Agent F: #{fixedAgent.getMove()}")
-		puts("Agent C: #{crowdAgent.getMove()}")
 =end
 
-		# Move to the next step
-		stepIdx += 1
+		stepIdx = 0
+		rActions = Array.new
+		fActions = Array.new
+		cActions = Array.new
+		while( stepIdx < @NumSteps )
+
+
+			# NOTE: Turnover position OPTION #1
+			if( stepIdx % @Tenure == 0 )
+				randAgent.rotateWorker()
+				fixedAgent.rotateWorker()
+				crowdAgent.rotateWorker()
+			end
+
+			rAct = randAgent.getMove()
+			fAct = fixedAgent.getMove()
+			cAct = crowdAgent.getMove()
+			rActions << rAct
+			fActions << fAct
+			cActions << cAct
+
+=begin
+			puts("\nAgent R: #{randAgent.getMove()}")
+			puts("Agent F: #{fixedAgent.getMove()}")
+			puts("Agent C: #{crowdAgent.getMove()}")
+=end
+
+			# Move to the next step
+			stepIdx += 1
+		end
+
+
+		# Move to the next round
+		idx += 1
+
+		# Calculate the entropy for each agent
+		rEnt = findEntropy(rActions)
+		fEnt = findEntropy(fActions)
+		cEnt = findEntropy(cActions)
+
+		# Output
+		#puts("Entropy-Crowd: #{cEnt}, Entropy-Rand: #{rEnt}")
+
+		# Record the result
+		rResultSet << rEnt
+		fResultSet << fEnt
+		cResultSet << cEnt
 	end
 
+	# Final answers
+	#puts("Average Entropy - Random: #{rResultSet.mean}")
+	#puts("Average Entropy - Fixed: #{fResultSet.mean}")
+	#puts("Average Entropy - Crowd: #{cResultSet.mean}")
 
-	# Move to the next round
-	idx += 1
+	# Print in CSV format
+	puts("#{@Tenure},#{rResultSet.mean},#{fResultSet.mean},#{cResultSet.mean}")
 
-	# Calculate the entropy for each agent
-	rEnt = findEntropy(rActions)
-	fEnt = findEntropy(fActions)
-	cEnt = findEntropy(cActions)
-
-	# Output
-	#puts("Entropy-Crowd: #{cEnt}, Entropy-Rand: #{rEnt}")
-
-	# Record the result
-	rResultSet << rEnt
-	fResultSet << fEnt
-	cResultSet << cEnt
-end
-
-# Final answers
-#puts("Average Entropy - Random: #{rResultSet.mean}")
-#puts("Average Entropy - Fixed: #{fResultSet.mean}")
-#puts("Average Entropy - Crowd: #{cResultSet.mean}")
-
-# Print in CSV format
-puts("#{@Tenure},#{rResultSet.mean},#{fResultSet.mean},#{cResultSet.mean}")
-
-@Tenure += @Incr
+	@Tenure += @Incr
 end
